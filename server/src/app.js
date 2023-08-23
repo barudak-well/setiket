a1a6f598dfcf36937c8530ac7a405e0c0ad17d20
Emirsyah <muhammademir48@gmail.com>
2023-08-23 22:03:12 +0700

    fix: change config for swagger spec and ops

--- server/src/app.js
+++ server/src/app.js
@@ -51,25 +51,24 @@ app.use(function (req, res, next) {
 
 dotenv.config();
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(specs, {
     explorer: true,
-    customSiteTitle: "Backend Generator",
-    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
-    customJs: [
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
-    ],
-    customCssUrl: [
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
-    ],
+    // Konfigurasi kalau misalnya masih salah:
+    // customSiteTitle: "Backend Generator",
+    // customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    // customJs: [
+    //   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+    //   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    // ],
+    // customCssUrl: [
+    //   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+    //   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+    //   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    // ],
   })
 );
 
--- server/src/config/swagger.config.js
+++ server/src/config/swagger.config.js
@@ -1,6 +1,5 @@
 const options = {
   definition: {
-    swagger: "2.0",
     openapi: "3.1.0",
     info: {
       title: "SeTiket Express API with Swagger",
