1f714d1d94562b2574db77c9150e7a61bf28882b
ariqhikari <ahidayatbia@gmail.com>
2023-08-23 09:17:12 +0700

    feat: add bookmark event

--- client/lib/src/features/common/presentation/event_detail/event_detail_state.dart
+++ client/lib/src/features/common/presentation/event_detail/event_detail_state.dart
@@ -2,19 +2,20 @@ import 'package:setiket/src/features/common/domain/event.dart';
 
 class EventDetailState {
   final Event? event;
+  final bool isBookmarkEvent;
   final int quantity;
-  EventDetailState({
-    this.event,
-    this.quantity = 1,
-  });
+  EventDetailState(
+      {this.event, this.quantity = 1, this.isBookmarkEvent = false});
 
   EventDetailState copyWith({
     Event? event,
     int? quantity,
+    bool? isBookmarkEvent,
   }) {
     return EventDetailState(
       event: event ?? this.event,
       quantity: quantity ?? this.quantity,
+      isBookmarkEvent: isBookmarkEvent ?? this.isBookmarkEvent,
     );
   }
 }
