1d1ea46323bc05e87489f4e6a65961a2738a64d9
Emirsyah <muhammademir48@gmail.com>
2023-08-23 21:37:30 +0700

    fix: add custom js and css to fix swagger

--- server/src/app.js
+++ server/src/app.js
@@ -57,7 +57,17 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(specs, {
     explorer: true,
-    customCssUrl: CSS_URL
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
   })
 );
 
