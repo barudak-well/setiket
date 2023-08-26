eeb030b5f2fb60f7695d17c472b8846f94462baa
codeyzx <yahyatruth@gmail.com>
2023-08-26 22:54:06 +0700

    fix: add bookmark functionality

--- client/lib/src/features/common/presentation/event_detail/event_detail_controller.dart
+++ client/lib/src/features/common/presentation/event_detail/event_detail_controller.dart
@@ -3,14 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:setiket/src/common_widgets/snack_bar/snack_bar_widget.dart';
 import 'package:setiket/src/constants/constants.dart';
 import 'package:setiket/src/features/application.dart';
+import 'package:setiket/src/features/common/domain/domain.dart';
 import 'package:setiket/src/features/presentation.dart';
 import 'package:setiket/src/services/services.dart';
 
 class EventDetailController extends StateNotifier<EventDetailState> {
   final CommonService _commonService;
   final HiveService _hiveService;
-  EventDetailController(this._commonService, this._hiveService)
-      : super(const EventDetailState());
+  EventDetailController(this._commonService, this._hiveService) : super(const EventDetailState());
 
   void getEventById(int id) async {
     state = state.copyWith(
@@ -43,8 +43,7 @@ class EventDetailController extends StateNotifier<EventDetailState> {
   void toggleBookmarkEvent(BuildContext context) {
     if (state.isBookmarkEvent) {
       _hiveService.deleteBookmarkEvent(state.event!.id);
-      appSnackBar(
-          context, ColorApp.green, 'Event successfully removed from bookmarks');
+      appSnackBar(context, ColorApp.green, 'Event successfully removed from bookmarks');
     } else {
       _hiveService.saveBookmarkEvent(state.event!);
       appSnackBar(context, ColorApp.green, 'Event successfully bookmarked');
@@ -54,10 +53,28 @@ class EventDetailController extends StateNotifier<EventDetailState> {
       isBookmarkEvent: _hiveService.isEventBookmark(state.event!.id),
     );
   }
+
+  bool isBookmarkEvent(int id) {
+    return _hiveService.isEventBookmark(id);
+  }
+
+  void toggleBookmarkEventById(BuildContext context, int id, Event event) {
+    final isBookmark = _hiveService.isEventBookmark(id);
+    if (isBookmark) {
+      _hiveService.deleteBookmarkEvent(id);
+      appSnackBar(context, ColorApp.green, 'Event successfully removed from bookmarks');
+    } else {
+      _hiveService.saveBookmarkEvent(event);
+      appSnackBar(context, ColorApp.green, 'Event successfully bookmarked');
+    }
+
+    state = state.copyWith(
+      isBookmarkEvent: _hiveService.isEventBookmark(id),
+    );
+  }
 }
 
-final eventDetailControllerProvider =
-    StateNotifierProvider<EventDetailController, EventDetailState>((ref) {
+final eventDetailControllerProvider = StateNotifierProvider<EventDetailController, EventDetailState>((ref) {
   final hiveService = ref.read(hiveServiceProvider);
   final commonService = ref.read(commonServiceProvider);
   return EventDetailController(commonService, hiveService);
